2c2a84d736d38df5570c795adf44fd51f6b313b3
smy <793501972@qq.com>
2013-07-23 10:45:10 +0800

    test

---
+++ app/models/line_item.rb
@@ -0,0 +1,12 @@
+class LineItem < ActiveRecord::Base
+  belongs_to :order
+  belongs_to :product
+  belongs_to :cart
+  attr_accessible :cart_id, :product_id
+  attr_accessible :product
+  attr_accessible :quantity
+  def total_price
+    product.price * quantity
+  end
+
+end
